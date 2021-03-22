8db693365a07d707abd5cb47ca7a13b478ef064a
anjaanzel <anja.anzel17@gmail.com>
2021-03-22 22:42:12 +0100

    Sign in logic

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul>
+        <li>
+            <router-link
+                :to="{
+                    name: 'Home'
+                }"
+            >
+                Home
+            </router-link>
+        </li>
+                <li>
+            <router-link
+                :to="{
+                    name: 'signin'
+                }"
+            >
+                Sign In
+            </router-link>
+        </li>
+        <li>
+            Anja Anzel
+        </li>
+        <li>
+            <router-link
+                :to="{
+                    name: 'dashboard'
+                }"
+            >
+                Dashboard
+            </router-link>
+        </li>
+        <li>
+            <a href="#">
+                Sign Out
+            </a>
+        </li>
+    </ul> 
+</template>
---
+++ src/views/SignIn.vue
@@ -0,0 +1,48 @@
+<template>
+    <form @submit.prevent="submit">
+        <div>
+            <label for="email">
+                Email
+            </label>
+
+            <input type="text" name="email" id="email" v-model="form.email">
+        </div>
+        <div>
+            <label for="password">
+                Password
+            </label>
+
+            <input type="password" name="password" id="password" v-model="form.password">
+        </div>
+        <div>
+            <button type="submit">
+                Sign In
+            </button>
+        </div>
+    </form> 
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'signin',
+  components: {
+    //
+  },
+  data () {
+      return {
+          form: {
+              email: '',
+              password: '',
+          }
+      }
+  },
+  methods: {
+          async submit () {
+              let response = axios.post('auth/signin', this.form)
+
+              console.log(response.data);
+          }
+      }
+}
+</script>
